297c97f40ed7bb476aca6f594bb812e4d534ee06
copequintana <manuel.quintana@t-softwaredev.com>
2022-02-14 18:57:30 -0700

    Pruebas

---
+++ js/Results.js
@@ -0,0 +1,25 @@
+function createTable(data) {
+    var array = parseResult(data)
+    var content = "";
+    array.forEach(function(row) {
+        content += "<tr>";
+        row.forEach(function(cell) {
+            content += "<td>" + cell + "</td>" ;
+        });
+        content += "</tr>";
+    });
+    document.getElementById("bodyDinamico").innerHTML = content;
+}
+
+function parseResult(result) {
+    console.log(data);
+    var resultArray = [];
+    result.split("\n").forEach(function(row) {
+        var rowArray = [];
+        row.split(",").forEach(function(cell) {
+            rowArray.push(cell);
+        });
+        resultArray.push(rowArray);
+    });
+    return resultArray;
+}
